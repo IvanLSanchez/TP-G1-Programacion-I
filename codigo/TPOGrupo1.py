3487e12df7f4515f538744a0a5c4dfc60f20e7e7
Ivan Lautaro Sanchez <100932334+IvanLSanchez@users.noreply.github.com>
2023-10-31 16:38:39 -0300

    finalizacion item 2

--- codigo/TPOGrupo1.py
+++ codigo/TPOGrupo1.py
@@ -1,3 +1,5 @@
+maximoDiccionario = lambda diccionario: max(diccionario.items(), key=lambda x: x[1]) #Devuelve la tupla con el valor maximo en factor de la clave
+
 def espectadoresAño(arch):
     """Crea un diccionario con la cantidad de entradas vendidas por año"""
     entradasAño = {}
@@ -7,18 +9,4 @@ def espectadoresAño(arch):
         entradasAño[año] = entradasAño.get(año, 0) + 1
     return entradasAño
 
-def mejorAño(arch):
-    """ejecuta el juego de acciones para oobtener el año con mayor cantidad de entradas vendidas"""
-    espectadoresAño(arch)
-
 #==================================================================================================
-try:
-    baseDeDatos = open("peliculas.txt","rt")
-    print(espectadoresAño(baseDeDatos))
-except OSError:
-    print("error al abrir aechivo")
-finally:
-    try:
-        baseDeDatos.close()
-    except:
-        pass
