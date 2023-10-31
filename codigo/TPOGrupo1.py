9cdef4a55796270632eb635b995c7118cd5b346f
gonzal07 <149263593+gonzal07@users.noreply.github.com>
2023-10-31 17:32:56 -0300

    finalizacion item1

--- codigo/TPOGrupo1.py
+++ codigo/TPOGrupo1.py
@@ -1,1 +1,19 @@
-#==================================================================================================
+#FUNCIONES
+## recopilacion-maximoDiccionario-porcentaje (maximoDiccionario(dicc)[1])*100)/sum(dicc.values())
+porcentaje = lambda x,y,z: (x*y)/z    #Porcentaje, x=maximo, y=100, z= el total 
+maximoDiccionario = lambda diccionario: max(diccionario.items(), key=lambda x: x[1]) #Maximo del diccionario
+def recopilacion(arch):
+    """recopilacion y creacion del diccionario"""
+    dic={}
+    for linea in arch:
+        nombre, año, sala=linea.split(";") #Desempaquetado de datos
+        dic[nombre]=dic.get(nombre,0) + 1 #agrega y acumula
+    return dic 
+        
+
+
+
+
+
+
+
