9c25a1fc81241076160bc5dd85cb26a45203dafc
Ivan Lautaro Sanchez <100932334+IvanLSanchez@users.noreply.github.com>
2023-10-31 16:10:37 -0300

    funcion espectadoresAño

--- codigo/TPOGrupo1.py
+++ codigo/TPOGrupo1.py
@@ -1,1 +1,24 @@
+def espectadoresAño(arch):
+    """Crea un diccionario con la cantidad de entradas vendidas por año"""
+    entradasAño = {}
+    for entrada in arch:
+        entrada = entrada.strip()
+        pelicula, año, sala = entrada.split(";")
+        entradasAño[año] = entradasAño.get(año, 0) + 1
+    return entradasAño
+
+def mejorAño(arch):
+    """ejecuta el juego de acciones para oobtener el año con mayor cantidad de entradas vendidas"""
+    espectadoresAño(arch)
+
 #==================================================================================================
+try:
+    baseDeDatos = open("peliculas.txt","rt")
+    print(espectadoresAño(baseDeDatos))
+except OSError:
+    print("error al abrir aechivo")
+finally:
+    try:
+        baseDeDatos.close()
+    except:
+        pass
