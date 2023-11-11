505aa0f78b4db5fbf9acc623cd3ccdd4985b946f
gonzal07 <149263593+gonzal07@users.noreply.github.com>
2023-11-11 17:20:07 -0300

    Update TPOGrupo1.py
    Corregi y agregue signos de puntuación.
    
    Saludos.
    
    
    Gonzalo.

--- codigo/TPOGrupo1.py
+++ codigo/TPOGrupo1.py
@@ -1,24 +1,24 @@
 # Funciones
 ## Filtracion
 def filtrarEntradasPorTitulo(arch,dic):
-    """llena un diccionario con la cantidad de entradas vendidas por titulo, 
-    sin importar el periodo y sala"""
+    """LLena un diccionario con la cantidad de entradas vendidas por titulo, 
+    sin importar el período y sala."""
     for linea in arch:
         linea = linea.strip()
         nombre, año, sala=linea.split(";") #Desempaquetado de datos
         dic[nombre]=dic.get(nombre,0) + 1 #agrega y acumula
 
 def filtrarEntradasPorAño(arch, entradasAño):
-    """llena un diccionario con la cantidad de entradas vendidas por año, 
-    sin importar el titulo y sala"""
+    """LLena un diccionario con la cantidad de entradas vendidas por año, 
+    sin importar el título y sala."""
     for entrada in arch:
         entrada = entrada.strip()
         pelicula, año, sala = entrada.split(";")
         entradasAño[año] = entradasAño.get(año, 0) + 1
 
 def espectadoresxSala(arch, espectadores):
-    """llena un arbol donde primero se establce los años y dentro la cantidad de 
-    entradas vendidas por sala, sin importar titulo"""
+    """LLena un árbol donde primero se establce los años y dentro la cantidad de 
+    entradas vendidas por sala, sin importar título."""
     for linea in arch:
         linea = linea.strip()
         pelicula, año, sala = linea.split(";")
@@ -27,8 +27,8 @@ def espectadoresxSala(arch, espectadores):
         espectadores[año][sala] = espectadores[año].get(sala,0) + 1
 
 def peliculaSala(arch,filtrado):
-    """llena un arbol donde primero se establce los titulos y dentro la cantidad de 
-    entradas vendidas por sala, sin importar año"""
+    """LLena un arbol donde primero se establce los títulos y dentro la cantidad de 
+    entradas vendidas por sala, sin importar año."""
     for linea in arch:
         linea = linea.strip()
         pelicula, año, sala = linea.split(";")
@@ -37,8 +37,8 @@ def peliculaSala(arch,filtrado):
         filtrado[pelicula][sala] = filtrado[pelicula].get(sala,0) + 1
 
 def salasTransmitidas(archivo, salas):
-    """llena un diccionario donde relacion titulos de peliculas con un conjunto de 
-    las salas en la que fue transmitida"""
+    """LLena un diccionario donde relacion títulos de peliculas con un conjunto de 
+    las salas en la que fue transmitida."""
     for linea in archivo:
         linea = linea.strip()
         pelicula, año, sala = linea.split(";")
@@ -46,21 +46,21 @@ def salasTransmitidas(archivo, salas):
             salas[pelicula] = set()
         salas[pelicula].add(sala)
 
-## Procesamiento
+## Procesamiento.
 reglasTresSimples = lambda x,y,z: (x*y)/z 
-    #Porcentaje
+    #Porcentaje.
 
 maximoDiccionario = lambda diccionario: max(diccionario.items(), key=lambda item: item[1]) 
-    #Devuelve la tupla con el valor maximo en factor de la clave
+    #Devuelve la tupla con el valor maximo en factor de la clave.
 
 ordenarDiccionarioPorKey = lambda diccionario: dict(sorted(diccionario.items(), key=lambda item: limpiarTitulo(item[0]))) 
-    #Ordena un diccionario en funcion de las claves
+    #Ordena un diccionario en funcion de las claves.
 
 mayorCantidadDeSalas = lambda diccionario: len(max(diccionario.values(), key=lambda x: len(x))) 
-    # Devuelve la longitud del iterable de mayor longitud de un diccionario
+    # Devuelve la longitud del iterable de mayor longitud de un diccionario.
 
 def quitarCaracteresEspeciales(palabra):
-    """Separa la palabra de los signos de puntuacion anteriores y posteriores"""
+    """Separa la palabra de los signos de puntuación anteriores y posteriores."""
     i = 0
     while i<len(palabra) and not palabra[i].isalnum():
         i += 1
@@ -71,7 +71,7 @@ def quitarCaracteresEspeciales(palabra):
     return palabra
 
 def quitarAcentos(palabra):
-    """quita los acentos de una palabra"""
+    """Quita los acentos de una palabra."""
     conTilde = "áéíóúÁÉÍÓÚ"
     sinTilde = "aeiouAEIOU"
     palabraSinAcentos = ""
@@ -84,21 +84,21 @@ def quitarAcentos(palabra):
     return palabraSinAcentos
 
 def limpiarTitulo(titulo):
-    """Ejecuta serie de comandos para dejar titulo sin caracteres especiales dejando de A-Z,1-9"""
+    """Ejecuta serie de comandos para dejar título sin caracteres especiales dejando de A-Z,1-9."""
     titulo = quitarCaracteresEspeciales(titulo)
     titulo = quitarAcentos(titulo)
     titulo = titulo.lower()
     return titulo
 
 def filtrarPeliculasMayorSalas(diccionario):
-    """Crea una lista con aquellas peliculas que tienen la mayor cantidad de salas transmitidas"""
+    """Crea una lista con aquellas películas que tienen la mayor cantidad de salas transmitidas."""
     mayor = mayorCantidadDeSalas(diccionario)
     filtrado = [key for key,value in diccionario.items() if len(value)==mayor]
     return filtrado
 
 def mejorCombo(diccionario):
-    """Calcula la mejor combinacion sala/pelicula sin importar el año, devuelve una tupla con el nombre de la pelicula y 
-    una tupla con la informacion de la sala"""
+    """Calcula la mejor combinación sala/película sin importar el año, devuelve una tupla con el nombre de la película y 
+    una tupla con la información de la sala."""
     diccionario = {key: maximoDiccionario(value) for key,value in diccionario.items()}
     combo = max(diccionario.items(), key=lambda item: item[1][1])
     return combo
@@ -108,25 +108,25 @@ def accion():
     while True:
         try:
             opcion = int(input("""
-¿Qué accion desea realizar?
-1) Pelicula mas taquillera
-2) Año con mayor cantidad de espectadores
-3) Cantidad de espectadores por sala cada año
-4) Cantidad de espectadores de cada pelicula
-5) Sala-pelicula mas convocante
-6) Pelicula con mayor cantidad de salas
-7) Salir
-Respuesta (indique numero de opcion): """))
+¿Qué acción desea realizar?
+1) Pelicula más taquillera.
+2) Año con mayor cantidad de espectadores.
+3) Cantidad de espectadores por sala cada año.
+4) Cantidad de espectadores de cada película.
+5) Sala-película más convocante.
+6) Película con mayor cantidad de salas.
+7) Salir.
+Respuesta (indique numero de opción): """))
             assert opcion>=1 and opcion<=7
             break
         except ValueError:
-            print("\nSolo se aceptan numeros. intente nuevamente\n")
+            print("\nSolo se aceptan numeros. Intente nuevamente.\n")
         except AssertionError:
-            print("\nSolo se permiten numeross del 1 al 7. intente nuevamente\n")
+            print("\nSolo se permiten numeross del 1 al 7. Intente nuevamente.\n")
     return opcion 
 
 def imprimirTitulos(tituloColumnas,ancho):
-    """Imprime serie de titulos en un ancho especifico de manera equitativa"""
+    """Imprime serie de títulos en un ancho específico de manera equitativa."""
     cantTitulos = len(tituloColumnas)
     print("-".center(ancho,"-"))
     for titulo in tituloColumnas:
@@ -135,7 +135,7 @@ def imprimirTitulos(tituloColumnas,ancho):
     print("-".center(ancho,"-"))
 
 def imprimirLista(lista,ancho):
-    """Imprime tabla de una columna"""
+    """Imprime tabla de una columna."""
     if len(lista) == 1:
         print(lista[0].center(ancho))
         print("-".center(ancho,"-"))
@@ -145,8 +145,8 @@ def imprimirLista(lista,ancho):
         imprimirLista(lista[1:],ancho)
 
 def imprimirTabla2ColumnasDict(diccionario, ancho, tituloColumnas):
-    """imprime un diccionario base (key, value) para hacerlo necesita el ancho que ocupara 
-    y los titulos para los dos encabezados"""
+    """Imprime un diccionario base (key, value) para hacerlo necesita el ancho que ocupará
+    y los títulos para los dos encabezados."""
     imprimirTitulos(tituloColumnas,ancho)
     for key,value in diccionario.items():
         print(key.center(ancho//2), end="")
@@ -154,8 +154,8 @@ def imprimirTabla2ColumnasDict(diccionario, ancho, tituloColumnas):
         print("-".center(ancho,"-"))
 
 def imprimirTabla3ColumnasDict(arbol,ancho, tituloColumnas):
-    """imprime un arbol base {key:{key:value},...} para hacerlo necesita el ancho que ocupara 
-    y los titulos para los dos encabezados"""
+    """Imprime un árbol base {key:{key:value},...} para hacerlo necesita el ancho que ocupará 
+    y los títulos para los dos encabezados."""
     imprimirTitulos(tituloColumnas,ancho)
     for key,value in arbol.items():
         ponerKey=True
@@ -172,7 +172,7 @@ def imprimirTabla3ColumnasDict(arbol,ancho, tituloColumnas):
 #=========================================================================================================
 try:
     baseDeDatos = open("peliculas2.txt","rt")
-    print("Bienvenidos al centro de estadisticas de cines mcPato.")
+    print("Bienvenidos al centro de estadísticas de cines mcPato.")
     opcion = accion()
     while opcion != 7:
         print()
@@ -181,23 +181,23 @@ try:
             filtrarEntradasPorTitulo(baseDeDatos, filtracion)
             masTaquillera=maximoDiccionario(filtracion)
             porcentaje=reglasTresSimples(masTaquillera[1],100,sum(filtracion.values()))
-            print(f"La pelicula mas taquillera fue '{masTaquillera[0]}' con una cantidad de {masTaquillera[1]} entradas vendidas. El porcentaje de ventas es de {porcentaje:.2f}%.")            
+            print(f"La película más taquillera fue '{masTaquillera[0]}' con una cantidad de {masTaquillera[1]} entradas vendidas. El porcentaje de ventas es de {porcentaje:.2f}%.")            
         elif opcion == 2:
             filtrarEntradasPorAño(baseDeDatos, filtracion)
             añoMasConvocante=maximoDiccionario(filtracion)
-            print(f"El año mas convocante fue: {añoMasConvocante[0]}.")
+            print(f"El año más convocante fue: {añoMasConvocante[0]}.")
         elif opcion == 3:
             espectadoresxSala(baseDeDatos, filtracion)
             filtracion=ordenarDiccionarioPorKey(filtracion)
-            imprimirTabla3ColumnasDict(filtracion,150,["año", "sala", "cantidad de espectadores"])
+            imprimirTabla3ColumnasDict(filtracion,150,["Año", "Sala", "Cantidad de espectadores"])
         elif opcion == 4:
             filtrarEntradasPorTitulo(baseDeDatos, filtracion)
             filtracion=ordenarDiccionarioPorKey(filtracion)
-            imprimirTabla2ColumnasDict(filtracion,100,["pelicula","cantidad de espectadores"])
+            imprimirTabla2ColumnasDict(filtracion,100,["Película","Cantidad de espectadores"])
         elif opcion == 5:
             peliculaSala(baseDeDatos, filtracion)
             combo=mejorCombo(filtracion) 
-            print(f"La mejor combinacion sala - pelicula es: '{combo[0]}' - sala {combo[1][0]}.")
+            print(f"La mejor combinación sala - película es: '{combo[0]}' - sala {combo[1][0]}.")
         else:
             salasTransmitidas(baseDeDatos, filtracion)
             filtrarPeliculasMayor=filtrarPeliculasMayorSalas(filtracion)
@@ -205,19 +205,19 @@ try:
             imprimirLista(filtrarPeliculasMayor,100)
         while True:
             try:
-                seguir = input("Desa realizar otra funcion? (Y/N): ")
+                seguir = input("¿Desa realizar otra función? (Y/N): ")
                 assert seguir.isalpha(), "\nSolo insertar letras. Intente nuevamente.\n"
                 seguir = seguir.upper()
-                assert seguir == "Y" or seguir == "N", "\nSolo insertar Y (yes) o N (no). Intente nuevamente.\n"
+                assert seguir == "Y" or seguir == "N", "\nSolo insertar ´Y´ (yes) o ´N´ (no). Intente nuevamente.\n"
                 opcion = accion() if seguir == "Y" else 7
                 if seguir == "Y":
                     baseDeDatos.seek(0)
                 break
             except AssertionError as mensaje:
                 print(mensaje)
-    print("\nGracias por visitar el centro de estadisticas de cines mcPato")
+    print("\nGracias por visitar el centro de estadísticas de cines mcPato.")
 except OSError:
-    print("error al abrir aechivo")
+    print("Error al abrir aechivo.")
 finally:
     try:
         baseDeDatos.close()
